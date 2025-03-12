23c81b00a899c138dbc5612b681ef66a25bff6e5
dreamer <dreamer.ashurov@gmail.com>
2025-03-12 15:37:27 +0500

    init commit

---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"strings"
+)
+
+type DocTF struct {
+	Name string
+	TF   map[string]float64
+}
+
+func initTFSlice(len int) []DocTF {
+	tfs := make([]DocTF, len)
+	for i := 0; i < len; i++ {
+		tfs[i].TF = make(map[string]float64)
+	}
+	return tfs
+}
+
+type Doc struct {
+	Name string
+	Text string
+}
+
+func initTestDocs() []Doc {
+	return []Doc{
+		{"doc1", "ananas banana apple"},
+		{"doc2", "apple orange apple banana apple"},
+		{"doc3", "grape ananas peach banana ananas"},
+		{"doc4", "mango pineapple banana"},
+		{"doc5", "strawberry blueberry raspberry ananas ananas"},
+		{"doc6", "banana apple peach"},
+		{"doc7", "pineapple mango orange"},
+		{"doc8", "grape banana strawberry"},
+		{"doc9", "blueberry raspberry ananas"},
+		{"doc10", "orange mango banana orange orange orange ananas ananas"},
+	}
+}
+
+func FindWordDoc(docs []Doc) map[string]int {
+	var wordInDocs = map[string]int{}
+	for _, doc := range docs {
+		mp := make(map[string]int)
+		docStrings := strings.Split(doc.Text, " ")
+		for _, s := range docStrings {
+			if _, ok := mp[s]; !ok {
+				wordInDocs[s] += 1
+			}
+			mp[s]++
+		}
+
+	}
+	return wordInDocs
+}
+
+func FindDocTF(docs []Doc, c chan []DocTF) {
+	defer close(c)
+	res := initTFSlice(len(docs))
+	for i, doc := range docs {
+		res[i].Name = doc.Name
+		mp := make(map[string]int)
+		docStrings := strings.Split(doc.Text, " ")
+		for _, s := range docStrings {
+			mp[s]++
+		}
+		for k, v := range mp {
+			res[i].TF[k] = float64(v) / float64(len(docStrings))
+		}
+	}
+	c <- res
+}
+
+func FindWordIdf(docs []Doc, c chan map[string]float64) map[string]float64 {
+	defer close(c)
+	wordInDocs := FindWordDoc(docs)
+	res := make(map[string]float64)
+	docsLen := len(docs)
+	for k, v := range wordInDocs {
+		res[k] = math.Log2(float64(docsLen) / float64(v))
+	}
+	c <- res
+	return res
+}
+
+type TfIdf struct {
+	Name  string
+	TfIdf []map[string]float64
+}
+
+type SearchResult struct {
+	DocName string
+	Sum     float64
+}
+
+func search(searchInfo string, tfIdfs []TfIdf) []SearchResult {
+	searchInf := strings.Split(searchInfo, " ")
+	var res []SearchResult
+	for _, idf := range tfIdfs {
+		tempSum := 0.0
+		for _, s := range searchInf {
+			for _, mp := range idf.TfIdf {
+				tempSum += mp[s]
+			}
+		}
+		res = append(res, SearchResult{DocName: idf.Name, Sum: tempSum})
+	}
+
+	for i := 0; i < len(res); i++ {
+		for j := 0; j < len(res)-1; j++ {
+			if res[j].Sum < res[j+1].Sum {
+				res[j], res[j+1] = res[j], res[j+1]
+			}
+		}
+	}
+
+	return res
+}
+
+func main() {
+	tf := make(chan []DocTF)
+	idf := make(chan map[string]float64)
+
+	data := initTestDocs()
+	go FindDocTF(data, tf)
+	go FindWordIdf(data, idf)
+	docsTf := <-tf
+	wordsIdf := <-idf
+	res := make([]TfIdf, len(docsTf))
+	for i, docTF := range docsTf {
+		res[i].Name = docTF.Name
+		for key, val := range docTF.TF {
+			res[i].TfIdf = append(res[i].TfIdf, map[string]float64{key: val * wordsIdf[key]})
+		}
+	}
+	//bytes, err := json.MarshalIndent(&res, "", " ")
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//os.WriteFile("model.json", bytes, 0644)
+
+	results := search("apple orange", res)
+	for _, result := range results {
+		log.Printf("Doc: %s is get ok whith %.4f reality\n=========\n", result.DocName, result.Sum)
+	}
+	for _, tfIdf := range res {
+		fmt.Printf("Doc: %s\nTF-IDF%+v\n===============\n", tfIdf.Name, tfIdf.TfIdf)
+	}
+}
---
+++ main.py
@@ -0,0 +1,139 @@
+import math
+
+import pymorphy2
+
+
+morph = pymorphy2.MorphAnalyzer()
+
+class Doc:
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
+
+
+def init_test_docs():
+    return [
+        Doc("doc1", "ananas banana apple"),
+        Doc("doc2", "apple orange apple banana apple"),
+        Doc("doc3", "grape ananas peach banana ananas"),
+        Doc("doc4", "mango pineapple banana"),
+        Doc("doc5", "strawberry blueberry raspberry ananas ananas"),
+        Doc("doc6", "banana apple peach"),
+        Doc("doc7", "pineapple mango orange"),
+        Doc("doc8", "grape banana strawberry"),
+        Doc("doc9", "blueberry raspberry ananas"),
+        Doc("doc10", "orange mango banana orange orange orange ananas ananas")
+    ]
+
+def init_test_docs_ru():
+    return [
+        Doc("doc1", "Яблоки полезны для здоровья. Они содержат витамины и антиоксиданты."),
+        Doc("doc2", "Космос – это бесконечное пространство, полное загадок и тайн."),
+        Doc("doc3", "Программирование – это процесс создания программ для компьютеров."),
+        Doc("doc4", "Путешествия помогают расширять кругозор и узнавать новые культуры."),
+        Doc("doc5", "Спорт укрепляет организм и помогает поддерживать хорошую физическую форму."),
+        Doc("doc6", "Искусственный интеллект меняет мир, улучшая технологии и автоматизируя процессы."),
+        Doc("doc7", "Музыка влияет на настроение человека и может вызывать сильные эмоции."),
+        Doc("doc8", "История – это наука, изучающая прошлое человечества."),
+        Doc("doc9", "Книги помогают развивать мышление и воображение, обогащая словарный запас."),
+        Doc("doc10", "Экология важна для сохранения природы и здоровья будущих поколений.")
+    ]
+
+
+
+def find_word_doc(docs):
+    word_in_docs = {}
+    for doc in docs:
+        seen_in_current_doc = set()
+        for token in doc.text.split():
+            if token not in seen_in_current_doc:
+                r_token = morph.parse(token)[0].normal_form
+                word_in_docs[r_token] = word_in_docs.get(r_token, 0) + 1
+                seen_in_current_doc.add(r_token)
+    return word_in_docs
+
+
+class DocTF:
+    def __init__(self, name):
+        self.name = name
+        self.tf = {}
+
+
+def find_doc_tf(docs):
+    results = []
+    for doc in docs:
+        doc_tf = DocTF(doc.name)
+        tokens = doc.text.split()
+        length = len(tokens)
+
+        freq_map = {}
+        for token in tokens:
+            r_token = morph.parse(token)[0].normal_form
+            freq_map[r_token] = freq_map.get(r_token, 0) + 1
+
+        for word, count in freq_map.items():
+            doc_tf.tf[word] = count / length
+
+        results.append(doc_tf)
+    return results
+
+
+def find_word_idf(docs):
+
+    word_in_docs = find_word_doc(docs)
+    total_docs = len(docs)
+    idf_map = {}
+    for word, doc_count in word_in_docs.items():
+        idf_map[word] = math.log2(total_docs / doc_count)
+    return idf_map
+
+
+class TfIdf:
+    def __init__(self, name):
+        self.name = name
+        self.tf_idf = []
+
+class SearcRes:
+    def __init__(self, doc_name, sum):
+        self.doc_name = doc_name
+        self.sum  = sum
+
+
+def search(search_info, tfIdfs):
+    serch_strings = search_info.split()
+    print(serch_strings)
+    results = []
+    for tfIdf in tfIdfs:
+        temp = 0
+        for serch in serch_strings:
+            for mp in tfIdf.tf_idf:
+                if mp.get(serch, 0) > 0:
+                    print(mp.get(serch, 0))
+    return results
+
+def main():
+    docs = init_test_docs_ru()
+
+    docs_tf = find_doc_tf(docs)
+
+    words_idf = find_word_idf(docs)
+
+    results = []
+    for doc_tf in docs_tf:
+        tfidf_obj = TfIdf(doc_tf.name)
+        for word, tf_val in doc_tf.tf.items():
+            tfidf_obj.tf_idf.append({word: tf_val * words_idf[word]})
+        results.append(tfidf_obj)
+
+    for item in results:
+        print(f"Document: {item.name}")
+        print("TF-IDF: ", item.tf_idf)
+        print("----------")
+    search_txt = 'здоровья яблоко'
+    search_res = search(search_txt, results)
+    for item in search_res:
+        print(f"document: {item.doc_name} reality: {item.sum}")
+
+
+if __name__ == "__main__":
+    main()
